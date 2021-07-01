02ef549ea5400b62660707a389ede0073a1240a1
Francisco Palacios <49413804+Edgard007@users.noreply.github.com>
2021-07-01 17:20:19 -0600

    💄 Improve home screen style

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,52 @@ import { criticalError } from "./Helpers/helpers";
 import { getAll as getAllAction } from "./Store/Actions/managers.actions";
 
 import styled from "styled-components";
-import { Table } from "antd";
+import { Table, Tooltip } from "antd";
+import {
+  PlusCircleTwoTone,
+  EditTwoTone,
+  DeleteTwoTone,
+} from "@ant-design/icons";
 
 const App = () => {
   //* ==> State <== *//
+  const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
 
   const getAll = async () => {
-    const result: any = await getAllAction();
-    const { ok, data } = result;
-    if (ok) {
-      setData(data);
-    } else criticalError("Error getting managers");
+    try {
+      setLoading(true); // Show Loading
+      const result: any = await getAllAction();
+      const { ok, data } = result;
+      if (ok) {
+        setData(data);
+      } else criticalError("Error getting managers");
+      setLoading(false); // Hide Loading
+    } catch (e) {
+      console.error("||* ==> Error getAll <== *||", e);
+      criticalError("Error getting managers");
+      setLoading(false); // Hide Loading
+    }
   };
 
   useEffect(() => {
     getAll();
   }, []);
 
+  const changeRecord = async (record: any) => {
+    console.log("record", record);
+  };
+
+  const deleteRecord = async (record: any) => {
+    console.log("record", record);
+  };
+
   const columns: Object[] = [
     {
       title: "ID",
       dataIndex: "id",
       key: "id",
+      align: "center",
     },
     {
       title: "Name",
@@ -42,6 +65,29 @@ const App = () => {
       title: "Year of release",
       dataIndex: "lanzamiento",
       key: "lanzamiento",
+      align: "center",
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      key: "actions",
+      align: "center",
+      render: (text: any, record: any) => (
+        <>
+          <Tooltip title="Edit record">
+            <EditTwoTone
+              className="icons"
+              onClick={() => changeRecord(record)}
+            />
+          </Tooltip>
+          <Tooltip title="Delete record">
+            <DeleteTwoTone
+              className="icons"
+              onClick={() => deleteRecord(record)}
+            />
+          </Tooltip>
+        </>
+      ),
     },
   ];
 
@@ -50,7 +96,14 @@ const App = () => {
       <div className="containerTitule">
         <span className="titule">Maintenance of managers</span>
       </div>
-      <Table dataSource={data} columns={columns} />;
+      <div className="containerTable">
+        <div className="headerTable">
+          <Tooltip title="Add new record">
+            <PlusCircleTwoTone className="iconAdd" />
+          </Tooltip>
+        </div>
+        <Table dataSource={data} columns={columns} loading={loading} />
+      </div>
     </Wrapper>
   );
 };
@@ -73,9 +126,30 @@ const Wrapper = styled.div`
     font-weight: bold;
   }
 
-  .ant-table-wrapper {
-    padding-left: 40px;
-    padding-right: 40px;
+  .icons {
+    cursor: pointer;
+    font-size: 20px;
+    margin-left: 20px;
+    color: #001e94 !important;
+  }
+
+  .containerTable {
+    margin-left: 40px;
+    margin-right: 40px;
+  }
+
+  .headerTable {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    margin-bottom: 20px;
+  }
+
+  .iconAdd {
+    cursor: pointer;
+    font-size: 25px;
+    margin-right: 20px;
+    color: #001e94 !important;
   }
 `;
 export default App;
